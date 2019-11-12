a338443e4238bd36c528d7b0b2f405197da8882b
bquint <bquint@gemini.edu>
2019-11-12 12:40:05 -0300

    conftest for gmos recipes now implements incremental mark

--- geminidr/gmos/recipes/sq/tests/conftest.py
+++ geminidr/gmos/recipes/sq/tests/conftest.py
@@ -2,5 +2,23 @@
 """
 Configuration file for tests in `geminidr.gmos.recipes.sq`.
 """
+
+import pytest
+
 # noinspection PyUnresolvedReferences
 from astrodata.testing import path_to_inputs, path_to_refs, path_to_outputs
+
+
+def pytest_runtest_makereport(item, call):
+    if "incremental" in item.keywords:
+        if call.excinfo is not None:
+            parent = item.parent
+            parent._previousfailed = item
+
+
+def pytest_runtest_setup(item):
+    if "incremental" in item.keywords:
+        previousfailed = getattr(item.parent, "_previousfailed", None)
+        if previousfailed is not None:
+            pytest.xfail("previous test failed ({})".format(previousfailed.name))
+
